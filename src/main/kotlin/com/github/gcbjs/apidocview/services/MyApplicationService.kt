15ddea6120b586a0ac962a6a39040896794515c2
GitHub Action <action@github.com>
2022-11-10 05:51:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/gcbjs/apidocview/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gcbjs/apidocview/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gcbjs.apidocview.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gcbjs.apidocview.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/gcbjs/apidocview/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gcbjs/apidocview/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gcbjs.apidocview.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gcbjs.apidocview.MyBundle
 
 class MyProjectService(project: Project) {
 
